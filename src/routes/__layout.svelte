ea27f38a072af2392464b1cde6804aeddb3bfe62
kgmmm <kgmofficial0@gmail.com>
2022-02-01 15:37:09 +0000

    css reset & basic route layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  console.log("layout loaded");
+</script>
+
+<main>
+  <slot/>
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr 400px;
+    grid-template-rows: 100%;
+    overflow: hidden;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,21 @@
-<h1>Wiki-CTF</h1>
+<article>
+  <h1>Wikipedia Capture the Flag!</h1>
+  <img src="/img/flag-globe.svg" alt="Flag Globe Logo">
+</article>
+<aside>
+  <h2>Aside</h2>
+</aside>
+
+<style>
+  article {
+    background: var(--red);
+  }
+  aside {
+    background: var(--blue);
+  }
+  img {
+    width: 15rem;
+    height: 15rem;
+    opacity: 20%;
+  }
+</style>
