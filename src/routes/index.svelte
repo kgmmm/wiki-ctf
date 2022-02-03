0ced333765595750b92e06d50715b6458b78eded
kgmmm <kgmofficial0@gmail.com>
2022-02-03 00:29:36 +0000

    index page with copy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,35 @@
 </script>
 
 <article>
-  <h1>Wikipedia Capture the Flag!</h1>
+  <div class="wrapper">
+    <div class="blurb">
+      <h1>Wikipedia Capture the Flag!</h1>
+      <p><span>Sign-in</span>
+      and invite a friend to play a head-to-head game of Wikipedia Capture the Flag!
+
+      <span>Plant your flag</span>
+      on a Wikipedia page of your choosing.
+
+      <span>Ready-up</span>
+      and race through Wikipedia links to capture your opponents flag.
+
+      <span>Hurry back</span>
+      to base with their flag to score a point, and let the next round begin.
+
+      <span>Watch out</span>
+      for interceptions! If a flag carrier is intercepted, any flags will sent back to base.
+
+      <span>Beware</span>
+      of dead ends! If you happen upon a page with no more links, you will be sent home, and lose a point.</p>
+    </div>
+    <div class="tips">
+      <h3>Tips</h3>
+      <ul>
+        <li>I know what you're thinking, but the back button isn't going to help you here.</li>
+        <li>A lot of page links have been disabled to keep you on the straight and narrow.</li>
+      </ul>
+    </div>
+  </div>
   <img src="/img/flag-globe.svg" alt="Flag Globe Logo" class="flag-globe">
 </article>
 <aside>
@@ -11,14 +39,65 @@
 </aside>
 
 <style>
-  img.flag-globe {
-    width: 15rem;
-    height: 15rem;
-    opacity: 20%;
+  article {
+    display: grid;
+    place-items: center;
+    background: var(--red);
+    position: relative;
+    z-index: -1;
   }
 
-  article {
-    background: blue; /* TODO stuff here*/
+  img.flag-globe {
+    position: absolute;
+    bottom: -10rem;
+    right: -16rem;
+    width: 70rem;
+    max-width: unset;
+    height: 70rem;
+    aspect-ratio: 1 / 1;
+    z-index: -1;
+    opacity: 25%;
+  }
+
+  div.wrapper {
+    margin-left: 2rem;
+    margin-right: 2rem;
+  }
+
+  div.blurb {
+    width: 100%;
+    max-width: 870px;
+  }
+  div.blurb h1 {
+    font-size: 3rem;
+    margin-bottom: 0.75em;
+  }
+  div.blurb span {
+    font-weight: 600;
+    font-size: 1.6rem;
+  }
+  div.blurb p {
+    font-size: 1.1rem;
+    line-height: 1.6rem;
+  }
+
+  div.tips {
+    margin-top: 3rem;
+    padding: 1em 1.5em;
+    width: max-content;
+    font-size: 0.95rem;
+    border: solid 1px #000;
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 10%);
+  }  
+  div.tips ul {
+    margin-block: 0.6em;
+    margin-left: 1em;
+  }
+  div.tips ul li {
+    list-style-type: '🏳️';
+    margin-bottom: 0.2em;
+    padding-left: 0.5em;
   }
 
   aside {
@@ -29,5 +108,6 @@
       "content"
       "user";
     background: var(--red);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 25%);
   }
 </style>
