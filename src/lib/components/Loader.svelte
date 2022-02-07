75a6e16f80f157a46ea7c06b8b63a349d2616a23
kgmmm <kgmofficial0@gmail.com>
2022-02-07 02:48:47 +0000

    loader component

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let size;
+</script>
+
+<div style="width: {size}px; height: {size}px; --b: {size / 8}px;"></div>
+
+<style>
+  div {
+    place-self: center;
+    border: solid var(--b) rgba(0, 0, 0, 40%);
+    border-top-color: var(--red);
+    border-radius: 50%;
+    animation: rotate 1s linear infinite;
+  }
+
+  @keyframes rotate {
+    to { transform: rotate(360deg); }
+  }
+</style>
