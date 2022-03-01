192b3ffc75b7287e87890aa6e7a941f993eb4c50
kgmmm <kgmofficial0@gmail.com>
2022-03-01 08:51:54 +0000

    some rough start for the scoreboard

--- src/lib/components/PlayingView.svelte
+++ src/lib/components/PlayingView.svelte
@@ -7,6 +7,19 @@
   export let opponentData;
   export let myData;
 
+  
+  function calcTime(time) {
+    var minutes = Math.floor(time / 60000);
+    var seconds = ((time % 60000) / 1000).toFixed(0);
+    return (
+      seconds == 60 ?
+      ("0" + minutes + 1) + ":00" :
+      "0" + minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+    );
+  }
+
+  $: roundTime = calcTime(gameState.roundTime);
+
   onMount(() => {
     splash.set({
       text: "GO",
@@ -31,7 +44,15 @@
   </div>
 
   <div class="scoreboard">
-    <h2>{gameState.roundTime}</h2>
+    <div class="opponentScore">
+      {opponentData.score}
+    </div>
+    <div class="timer">
+      {roundTime}
+    </div>
+    <div class="myScore">
+      {myData.score}
+    </div>
   </div>
 
   <div class="me">
@@ -44,12 +65,62 @@
   div.container {
     grid-area: content;
     display: grid;
-    grid-template-rows: 1fr 100px 1fr;
+    grid-template-rows: 1fr 89px 1fr;
   }
 
-  div.opponent, div.me, div.scoreboard {
+  div.opponent, div.me, div.scoreboard, div.timer, div.opponentScore, div.myScore {
     display: grid;
     place-items: center;
     text-align: center;
+  }
+
+  div.scoreboard {
+    color: #fff;
+    font-family: 'Courier New', Courier, monospace;
+    background: linear-gradient(240deg, var(--blue) 50%, var(--red) 0%);
+    padding-block: 7px;
+    grid-template-columns: 75px 33px 1fr 33px 75px;
+    grid-template-rows: 1fr 1fr;
+    gap: 7px;
+    grid-template-areas:
+      "mScore obtn timer mbtn oScore"
+      "mScore obtn timer mbtn oScore";
+  }
+
+  div.timer, div.opponentScore, div.myScore {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 20%);
+    border-radius: 5px;
+  }
+
+  div.opponentScore {
+    grid-area: oScore;
+    border-radius: 0 5px 5px 0;
+    font-size: 1.5rem;
+    position: relative;
+  }
+  div.myScore {
+    grid-area: mScore;
+    border-radius: 5px 0 0 5px;
+    font-size: 1.5rem;
+    position: relative;
+  }
+
+  div.opponentScore::after, div.myScore::after {
+    content: "points";
+    position: absolute;
+    bottom: 3px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 0.7rem;
+    text-transform: uppercase;
+    color: #fff;
+    opacity: 70%;
+  }
+
+  div.timer {
+    grid-area: timer;
+    font-size: 1.5rem;
   }
 </style>
