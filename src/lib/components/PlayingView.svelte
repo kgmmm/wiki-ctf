7b4faef964f97dc1041a46ae1e83df26fe870a2d
kgmmm <kgmofficial0@gmail.com>
2022-03-02 07:17:09 +0000

    active classes checks on icons. adjusted text sizing slightly

--- src/lib/components/PlayingView.svelte
+++ src/lib/components/PlayingView.svelte
@@ -43,12 +43,12 @@
         opponent's base
       </div>
       <div class="display">
-        <div class="icon">
+        <div class="icon" class:active={myData.carrying}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
             <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
           </svg>
         </div>
-        <h1 style="font-size: Clamp(0.75rem, {320 / opponentData['base'].length / 8 + 'rem'}, 1.2rem);">{opponentData.base}</h1>
+        <h1 style="font-size: Clamp(0.75rem, {310 / opponentData['base'].length / 8 + 'rem'}, 1.2rem);">{opponentData.base}</h1>
       </div>
     </div>
     <div class="oLoc">
@@ -61,7 +61,7 @@
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
           </svg>
         </div>
-        <h1 style="font-size: Clamp(0.75rem, {320 / opponentData['location'].length / 8 + 'rem'}, 1.2rem);">{opponentData.location}</h1>
+        <h1 style="font-size: Clamp(0.75rem, {310 / opponentData['location'].length / 8 + 'rem'}, 1.2rem);">{opponentData.location}</h1>
       </div>
     </div>
   </div>
@@ -70,7 +70,7 @@
     <div class="myScore">
       {myData.score}
     </div>
-    <span class="mBase">
+    <span class="mBase" class:active={opponentData.carrying}>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
         <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
       </svg>
@@ -88,7 +88,7 @@
         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
       </svg>
     </span>
-    <span class="oBase">
+    <span class="oBase" class:active={myData.carrying}>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
         <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
       </svg>
@@ -109,7 +109,7 @@
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
           </svg>
         </div>
-        <h1 style="font-size: Clamp(0.75rem, {320 / myData['location'].length / 8 + 'rem'}, 1.2rem);">{myData.location}</h1>
+        <h1 style="font-size: Clamp(0.75rem, {310 / myData['location'].length / 8 + 'rem'}, 1.2rem);">{myData.location}</h1>
       </div>
     </div>
     <div class="mBase">
@@ -117,12 +117,12 @@
         your base
       </div>
       <div class="display">
-        <div class="icon">
+        <div class="icon" class:active={opponentData.carrying}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
             <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
           </svg>
         </div>
-        <h1 style="font-size: Clamp(0.75rem, {320 / myData['base'].length / 8 + 'rem'}, 1.2rem);">{myData.base}</h1>
+        <h1 style="font-size: Clamp(0.75rem, {310 / myData['base'].length / 8 + 'rem'}, 1.2rem);">{myData.base}</h1>
       </div>
     </div>
   </div>
