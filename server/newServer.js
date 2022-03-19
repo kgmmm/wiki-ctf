3b044019283fc59f6dae885dc096383b9bb0d2e4
kgmmm <kgmofficial0@gmail.com>
2022-03-19 14:45:56 +0000

    reordered server files into server folder, began moving things over from old server file

--- server/newServer.js
+++ server/newServer.js
@@ -1,7 +1,17 @@
 import { handler } from './build/handler.js';
 import express from 'express';
 
+import http from "http";
+import { Server as SocketIO } from "socket.io";
+
 const app = express();
+
+const server = http.createServer();
+const io = new SocketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(handler); // let SvelteKit handle everything else, including serving prerendered pages and static assets
 
