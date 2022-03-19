615ce391a96664b5c692449fc1539b2a5f9a3a4e
kgmmm <kgmofficial0@gmail.com>
2022-03-19 14:41:02 +0000

    basic setup with handler.js from adapter-node github readme

--- newServer.js
+++ newServer.js
@@ -1,1 +1,10 @@
-// newServer.js
+import { handler } from './build/handler.js';
+import express from 'express';
+
+const app = express();
+
+app.use(handler); // let SvelteKit handle everything else, including serving prerendered pages and static assets
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('server listening');
+});
