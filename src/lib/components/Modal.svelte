2639df4b0fe181b4bdd33a02150537b1dd28a15f
kgmmm <kgmofficial0@gmail.com>
2022-03-21 17:12:33 +0000

    added Map to roundEnd and gameEnd Modal, styling needs a lot of work

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -2,6 +2,7 @@
   import { createEventDispatcher } from "svelte";
   import { fly, fade } from "svelte/transition";
   import Loader from "./Loader.svelte";
+  import Map from "./Map.svelte";
 
   export let gameState;
   export let myData;
@@ -38,54 +39,57 @@
 </script>
 
 <div class="container" in:fly={{y: 50, duration: 250}}>
-  {#if gameState.stage == "roundend"}
-    {#if gameState.lastRoundResult == "time"}
-      <h2>Time ran out!</h2>
-      <span>...round is a draw.</span>
-    {:else}
-      <div class="profilepic">
-        <img src={roundWinner.profilePic} alt={`${roundWinner.userID}'s profile picture`} class="profilepic" referrerpolicy="no-referrer">
+  <div class="endscreen">
+    {#if gameState.stage == "roundend"}
+      {#if gameState.lastRoundResult == "time"}
+        <h2>Time ran out!</h2>
+        <span>...round is a draw.</span>
+      {:else}
+        <div class="profilepic">
+          <img src={roundWinner.profilePic} alt={`${roundWinner.userID}'s profile picture`} class="profilepic" referrerpolicy="no-referrer">
+        </div>
+        <h1 class="char-limit">{roundWinner.displayName}</h1><span>...won&nbsp;this&nbsp;round!</span>
+      {/if}
+      <h3>{gameState.players[0].score} - {gameState.players[1].score}</h3>
+      <h4>Ready for the next round?</h4>
+      <div class="buttons">
+        <button class="disconnect" title="Disconnect" on:click={handleDisconect}>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
+            <path d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z" fill="currentColor" />
+          </svg>
+        </button>
+        <button class="ready" on:click={handleReady}>
+          {#if readyLoader}
+            <Loader size="35" color="#fff" />
+          {:else}
+            Ready
+          {/if}
+        </button>
       </div>
-      <h1 class="char-limit">{roundWinner.displayName}</h1><span>...won&nbsp;this&nbsp;round!</span>
-    {/if}
-    <h3>{gameState.players[0].score} - {gameState.players[1].score}</h3>
-    <h4>Ready for the next round?</h4>
-    <div class="buttons">
-      <button class="disconnect" title="Disconnect" on:click={handleDisconect}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
-          <path d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z" fill="currentColor" />
-        </svg>
-      </button>
-      <button class="ready" on:click={handleReady}>
-        {#if readyLoader}
-          <Loader size="35" color="#fff" />
-        {:else}
+    {:else if gameState.stage == "gameend"}
+      <div class="profilepic">
+        <img src={gameWinner.profilePic} alt={`${gameWinner.userID}'s profile picture`} class="profilepic" referrerpolicy="no-referrer">
+      </div>
+      <h1 class="char-limit">{gameWinner.displayName}</h1><span>...won&nbsp;the&nbsp;game!</span>
+      <h3>{gameState.players[0].score} - {gameState.players[1].score}</h3>
+      {#if didIWin}
+        <h4>Congratulations!</h4>
+      {:else}
+        <h4>Be a little quicker next game.</h4>
+      {/if}
+      <div class="buttons">
+        <button class="disconnect" title="Disconnect" on:click={handleDisconect}>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
+            <path d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z" fill="currentColor" />
+          </svg>
+        </button>
+        <button class="ready" disabled>
           Ready
-        {/if}
-      </button>
-    </div>
-  {:else if gameState.stage == "gameend"}
-    <div class="profilepic">
-      <img src={gameWinner.profilePic} alt={`${gameWinner.userID}'s profile picture`} class="profilepic" referrerpolicy="no-referrer">
-    </div>
-    <h1 class="char-limit">{gameWinner.displayName}</h1><span>...won&nbsp;the&nbsp;game!</span>
-    <h3>{gameState.players[0].score} - {gameState.players[1].score}</h3>
-    {#if didIWin}
-      <h4>Congratulations!</h4>
-    {:else}
-      <h4>Be a little quicker next game.</h4>
+        </button>
+      </div>
     {/if}
-    <div class="buttons">
-      <button class="disconnect" title="Disconnect" on:click={handleDisconect}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
-          <path d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z" fill="currentColor" />
-        </svg>
-      </button>
-      <button class="ready" disabled>
-        Ready
-      </button>
-    </div>
-  {/if}
+  </div>
+  <Map />
 </div>
 <div class="shadow" in:fade={{duration: 300}}></div>
 
@@ -98,12 +102,11 @@
   }
 
   div.container {
-    padding: 33px;
-    width: 500px;
-    height: 380px;
+    padding: 7px;
+    height: 400px;
     color: #000;
     background: #fff;
-    background: linear-gradient(-120deg, rgba(0, 0, 0, 5%) 0% 10%, transparent 10% 90%, rgba(0, 0, 0, 5%) 90% 100%), #fff;
+    /* background: linear-gradient(-120deg, rgba(0, 0, 0, 5%) 0% 10%, transparent 10% 90%, rgba(0, 0, 0, 5%) 90% 100%), #fff; */
     border-radius: 10px;
     box-shadow: 0 4px 15px rgba(0, 0, 0, 20%);
     position: absolute;
@@ -111,6 +114,13 @@
     transform: translate(-50%, -50%);
     z-index: 900;
     text-align: center;
+    display: grid;
+    place-items: center;
+    grid-template-columns: 500px 320px;
+    gap: 7px;
+  }
+
+  div.endscreen {
     display: grid;
     place-items: center;
   }
