ecf6dcb714cc8b5db6becc1fbfedcddd3043fe16
kgmmm <kgmofficial0@gmail.com>
2022-03-22 23:27:39 +0000

    temporary testing environment for Map with dummy data

---
+++ src/lib/mapTestData.js
@@ -0,0 +1,547 @@
+const dataSamples = { small: [
+  [
+      "Lopado­temacho­selacho­galeo­kranio­leipsano­drim­hypo­trimmato­silphio­karabo­melito­katakechy­meno­kichl­epi­kossypho­phatto­perister­alektryon­opte­kephallio­kigklo­peleio­lagoio­siraio­baphe­tragano­pterygon",
+      "Tax Relief, Unemployment Insurance Reauthorization, and Job Creation Act of 2010",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      0
+  ],
+  [
+      "Cneoridium dumosum (Nuttall) Hooker F. Collected March 26, 1960, at an Elevation of about 1450 Meters on Cerro Quemazón, 15 Miles South of Bahía de Los Angeles, Baja California, México, Apparently for a Southeastward Range Extension of Some 140 Miles",
+      "Joe Biden",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      1
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      1
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      1
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      1
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      1
+  ]
+], medium: [
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      1
+  ]
+], large: [
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      1
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Barack Obama",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Barack Obama",
+      3
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      3
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Barack Obama",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Barack Obama",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Donald Trump",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      0
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Joe Biden",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      2
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Joe Biden",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Kamala Harris",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Kamala Harris",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Kamala Harris",
+      3
+  ],
+  [
+      "Donald Trump",
+      "Kamala Harris",
+      3
+  ],
+  [
+      "Tax Relief, Unemployment Insurance Reauthorization, and Job Creation Act of 2010",
+      "Kamala Harris",
+      3
+  ]
+]
+}
+
+export default dataSamples;
---
+++ src/routes/maptest.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Map from "$lib/components/Map.svelte";
+  import { mapData } from "$lib/stores/mapData";
+  import dataSamples from "$lib/mapTestData";
+
+  function setMapData(data) {
+    mapData.set(data);
+  }
+</script>
+
+<div class="container">
+  <Map />
+</div>
+
+<div class="buttons">
+  <button on:click={() => setMapData(dataSamples.small)}>small</button>
+  <button on:click={() => setMapData(dataSamples.medium)}>medium</button>
+  <button on:click={() => setMapData(dataSamples.large)}>large</button>
+</div>
+
+<style>
+  div.container {
+    width: 330px;
+    height: 330px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0 0 200px #000;
+
+    overflow-y: scroll;
+  }
+  div.buttons {
+    position: fixed;
+    bottom: 200px;
+    left: 100px;
+  }
+</style>
