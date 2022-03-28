e7a76aba304c04b853405cab4bddfd146a4329c1
kgmmm <kgmofficial0@gmail.com>
2022-03-28 02:57:19 +0100

    added ScoreboardIcon components, removed active glow from other icons in displays

--- src/lib/components/PlayingView.svelte
+++ src/lib/components/PlayingView.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { splash } from "$lib/stores/splash";
+  import ScoreboardIcon from "./ScoreboardIcon.svelte";
 
 
   export let gameState;
@@ -43,7 +44,7 @@
         opponent's base
       </div>
       <div class="display">
-        <div class="icon" class:active={myData.carrying}>
+        <div class="icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
             <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
           </svg>
@@ -70,29 +71,13 @@
     <div class="myScore">
       {myData.score}
     </div>
-    <span class="mBase" class:active={opponentData.carrying}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
-        <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
-      </svg>
-    </span>
-    <span class="mLoc">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-      </svg>
-    </span>
+    <ScoreboardIcon area="mBase" type="flag" active={opponentData.carrying} />
+    <ScoreboardIcon area="mLoc" type="location" active={false} />
     <div class="timer">
       {roundTime}
     </div>
-    <span class="oLoc">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-      </svg>
-    </span>
-    <span class="oBase" class:active={myData.carrying}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
-        <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
-      </svg>
-    </span>
+    <ScoreboardIcon area="oLoc" type="location" active={false} />
+    <ScoreboardIcon area="oBase" type="flag" active={myData.carrying} />
     <div class="opponentScore">
       {opponentData.score}
     </div>
@@ -117,7 +102,7 @@
         your base
       </div>
       <div class="display">
-        <div class="icon" class:active={opponentData.carrying}>
+        <div class="icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
             <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
           </svg>
@@ -129,22 +114,6 @@
 </div>
 
 <style>
-  .active {
-    box-shadow: 0 0 10px yellow;
-    animation: active .5s infinite alternate-reverse;
-    position: relative;
-    z-index: 1;
-  }
-
-  @keyframes active {
-    from {
-      box-shadow: 0 0 5px yellow;
-    }
-    to {
-      box-shadow: 0 0 10px yellow;
-    }
-  }
-
   div.container {
     grid-area: content;
     display: grid;
@@ -267,37 +236,8 @@
     grid-template-rows: 1fr 1fr;
     gap: 7px;
     grid-template-areas:
-      "mScore mBase timer oLoc oScore"
-      "mScore mLoc timer oBase oScore";
-  }
-
-  div.scoreboard span {
-    width: 100%;
-    height: 100%;
-    color: #fff;
-    background: rgba(0, 0, 0, 10%);
-    border: solid 1px rgba(0, 0, 0, 15%);
-    border-radius: 5px;
-    display: grid;
-    place-items: center;
-  }
-  div.scoreboard span svg {
-    fill: currentColor;
-    width: 1em;
-    height: 1em;
-    font-size: 1.2rem;
-  }
-  span.mBase {
-    grid-area: mBase;
-  }
-  span.mLoc {
-    grid-area: mLoc;
-  }
-  span.oBase {
-    grid-area: oBase;
-  }
-  span.oLoc {
-    grid-area: oLoc;
+      "mScore mLoc timer oBase oScore"
+      "mScore mBase timer oLoc oScore";
   }
 
   div.timer, div.opponentScore, div.myScore {
