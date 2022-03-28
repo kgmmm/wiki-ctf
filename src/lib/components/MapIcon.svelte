61f71137e9aab150de65f77f6002f6dd153d28d7
kgmmm <kgmofficial0@gmail.com>
2022-03-28 06:58:02 +0100

    added trophy Type icon and toggling trophy class to apply accent shadow

--- src/lib/components/MapIcon.svelte
+++ src/lib/components/MapIcon.svelte
@@ -17,7 +17,7 @@
   let focussed = false;
 </script>
 
-<div use:popperRef class="icon" class:joint tabindex="0"
+<div use:popperRef class="icon" class:joint class:trophy={type === "trophy"} tabindex="0"
      on:mouseenter={() => { if(!focussed) showToolTip = true; }}
      on:mouseleave={() => { if(!focussed) showToolTip = false; }}
      on:focus={() => { focussed = true; showToolTip = true; }}
@@ -29,6 +29,10 @@
   {:else if type == "location"}
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
       <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+    </svg>
+  {:else if type == "trophy"}
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
+      <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
     </svg>
   {/if}
 </div>
@@ -60,6 +64,10 @@
     height: 1em;
     font-size: 0.9rem;
     fill: currentColor;
+  }
+
+  div.icon.trophy {
+    box-shadow: 0 0 3px var(--accent);
   }
 
   div.icon:focus + div.display {
