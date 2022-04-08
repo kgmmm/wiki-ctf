50c824fd20d90108f64aaa9d8fd838cfb08753ac
kgmmm <kgmofficial0@gmail.com>
2022-04-08 17:32:44 +0100

    clicking on a map icon now changes the text in the tooltip to a hyperlink for the relevant wikipedia article

--- src/lib/components/MapIcon.svelte
+++ src/lib/components/MapIcon.svelte
@@ -15,13 +15,15 @@
 
   let showToolTip = false;
   let focussed = false;
+  let linkClick = false;
 </script>
 
 <div use:popperRef class="icon" class:joint class:trophy={type === "trophy"} tabindex="0"
      on:mouseenter={() => { if(!focussed) showToolTip = true; }}
      on:mouseleave={() => { if(!focussed) showToolTip = false; }}
      on:focus={() => { focussed = true; showToolTip = true; }}
-     on:blur={() => { focussed = false; showToolTip = false; }}>
+     on:blur={() => { focussed = false; showToolTip = false; linkClick = false; }}
+     on:click={() => { linkClick = true; }}>
   {#if type == "flag"}
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
       <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
@@ -39,7 +41,13 @@
 {#if showToolTip}
   <div use:popperContent={popperOptions} class="display" style:--font-size={135 / text.length / 8 + 'rem'}
        in:fade={{ duration: 100 }}
-       out:fade={{ duration: 100 }}>{text}</div>
+       out:fade={{ duration: 100 }}>
+    {#if linkClick}
+      <a href={`https://en.wikipedia.org/wiki/${text}`} target="_blank" rel="external">{text}</a>
+    {:else}
+      {text}
+    {/if}
+  </div>
 {/if}
 
 <style>
@@ -106,6 +114,17 @@
     border-right: solid 10px transparent;
     border-top-color: rgba(45, 46, 50, 90%);
     pointer-events: none;
+  }
+
+  div.display a {
+    color: #578fe9;
+    text-decoration: underline;
+  }
+  div.display a:hover {
+    color: #73a0e9;
+  }
+  div.display a:active {
+    color: #faa700;
   }
 
   div.icon.joint {
